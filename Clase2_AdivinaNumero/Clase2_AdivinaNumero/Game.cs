e5885a83ad724ed8d0566dd66e5dac91eca99b00
Miguel Moronta <miguel@moronta.com.do>
2020-01-15 15:18:09 -0400

    Ejercicio en Clases #1 - Adivina un número.

---
+++ Clase2_AdivinaNumero/Clase2_AdivinaNumero/Game.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2_AdivinaNumero
+{
+    public class Game
+    {
+        #region "Enums"
+        public enum eGameState
+        {
+            Starting,
+            Playing,
+            Over
+        }
+
+        public enum AttemptResult
+        {
+            Guessed,
+            Lower,
+            Greater
+        }
+        #endregion
+
+        #region "Attributes"
+        const int MIN = 1, MAX = 1001;
+        const string DEFAULTPATH = "score.txt";
+        public int SecretNumber { get; set; }
+        public eGameState CurrentState { get; set; }
+        public int LastTry { get; set; }
+        public int Attempts { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan TimeSpent
+        {
+            get
+            {
+                return EndTime.Subtract(StartTime);
+            }
+        }
+        public string ScorePath { get; set; }
+        #endregion
+
+        #region "Behaviours"
+        public void GameInit()
+        {
+            ScorePath = DEFAULTPATH;
+            SecretNumber = GenerateNumber(MIN, MAX);
+            CurrentState = eGameState.Starting;
+            Attempts = 0;
+            StartTime = DateTime.Now;
+        }
+
+        private int GenerateNumber(int min, int max)
+        {
+            Random rnd = new Random();
+            return rnd.Next(min, max);
+        }
+
+        public AttemptResult CheckIfGuessed()
+        {
+            Attempts++;
+            if (LastTry == SecretNumber)
+            {
+                EndTime = DateTime.Now;
+                return AttemptResult.Guessed;
+            }
+            else if (LastTry > SecretNumber)
+                return AttemptResult.Greater;
+            else
+                return AttemptResult.Lower;
+        }
+
+        public void SaveState()
+        {
+            /*using (FileStream fstream = File.Open(ScorePath, FileMode.Append))
+            {
+                UnicodeEncoding unicodeEncoding = new UnicodeEncoding();
+                var buffer = unicodeEncoding.GetBytes($"Tiempo: {TimeSpent.TotalSeconds} - Intentos: {Attempts}");
+                fstream.Write(buffer, 0, buffer.Length);
+            }*/
+            var streamWriter = File.AppendText(ScorePath);
+            streamWriter.Write($"Tiempo: {TimeSpent.TotalSeconds} - Intentos: {Attempts}\n");
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+
+        public string ReadScores()
+        {
+            return File.ReadAllText(ScorePath);
+        }
+        #endregion
+    }
+}
---
+++ Clase2_AdivinaNumero/Clase2_AdivinaNumero/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clase2_AdivinaNumero
+{
+    class Program
+    {
+        public static Game CurrentGame;
+        static Thread inputThread;
+
+        static void Main(string[] args)
+        {
+            CurrentGame = new Game();
+            CurrentGame.GameInit();
+            inputThread = new Thread(Input);
+            inputThread.Start();
+
+            Console.Write(CurrentGame.ReadScores());
+
+            while(CurrentGame.CurrentState != Game.eGameState.Over)
+            {
+                switch(CurrentGame.CurrentState)
+                {
+                    case Game.eGameState.Starting:
+                        Console.WriteLine("Digite un valor entre 1 y 1000:");
+                        CurrentGame.CurrentState = Game.eGameState.Playing;
+                        break;
+                    case Game.eGameState.Playing:
+                        if (CurrentGame.LastTry == 0)
+                            continue;
+
+                        // Check if secret number is guessed...
+                        switch(CurrentGame.CheckIfGuessed())
+                        {
+                            case Game.AttemptResult.Greater:
+                                Console.WriteLine("El número secreto es menor.");
+                                break;
+                            case Game.AttemptResult.Lower:
+                                Console.WriteLine("El número secreto es mayor.");
+                                break;
+                            default:
+                                Console.WriteLine("¡HA ADIVINADO!");
+                                CurrentGame.CurrentState = Game.eGameState.Over;
+                                break;
+                        }
+
+                        if (CurrentGame.CurrentState != Game.eGameState.Over)
+                            Console.WriteLine("Digite otro valor: ");
+
+                        // And reset the last try.
+                        CurrentGame.LastTry = 0;
+                        break;
+                }
+            }
+            inputThread.Abort();
+            inputThread.Join();
+            CurrentGame.SaveState();
+            Console.WriteLine($"Ha adivinado en {CurrentGame.Attempts} intentos.");
+            Console.WriteLine($"Ha tomado {CurrentGame.TimeSpent.TotalSeconds} segundos.");
+            Console.WriteLine("Gracias por jugar.");
+            Console.ReadKey();
+        }
+
+        static void Input()
+        {
+            int _currentValue = 0;
+            while(CurrentGame.CurrentState != Game.eGameState.Over)
+            {
+                _currentValue = Convert.ToInt32(Console.ReadLine());
+                CurrentGame.LastTry = _currentValue;
+            }
+        }
+    }
+}
